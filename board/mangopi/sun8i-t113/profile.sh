983c58ac08770fe88d7725dbfbb278bf5ff405b5
vvpnet <v.v.p.net@mail.ru>
2023-04-17 11:16:38 +0600

    add config file mangopi_sun8i-t113 with qt

---
+++ board/mangopi/sun8i-t113/profile.sh
@@ -0,0 +1,10 @@
+export LANG=ru_RU.UTF-8
+export GST_INSPECT_NO_COLORS=1
+export PATH=/root:$PATH
+export TSLIB_TSDEVICE=/dev/input/event0
+export QT_QPA_GENERIC_PLUGINS=tslib:/dev/input/event0
+export QT_QPA_PLATFORM=linuxfb
+export QT_QPA_FB_TSLIB=1
+export QT_QPA_FB_DRM=1
+
+# export FB_MULTI_BUFFER=2
