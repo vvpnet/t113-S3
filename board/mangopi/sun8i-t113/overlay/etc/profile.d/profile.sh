8ee00ae090f5e83a39fe34b2eb3944c0e7570f4e
vvpnet <v.v.p.net@mail.ru>
2023-06-15 08:32:29 +0600

    update env profile

--- board/mangopi/sun8i-t113/overlay/etc/profile.d/profile.sh
+++ board/mangopi/sun8i-t113/overlay/etc/profile.d/profile.sh
@@ -1,10 +1,18 @@
 export LANG=ru_RU.UTF-8
-export GST_INSPECT_NO_COLORS=1
 export PATH=/root:$PATH
-export TSLIB_TSDEVICE=/dev/input/event0
-export QT_QPA_GENERIC_PLUGINS=tslib:/dev/input/event0
-export QT_QPA_PLATFORM=linuxfb
-export QT_QPA_FB_TSLIB=1
+export GST_INSPECT_NO_COLORS=1
+
 export QT_QPA_FB_DRM=1
+export QT_QPA_PLATFORM=linuxfb
+#Available devices:
+#/dev/input/event0:      rotary@0
+#/dev/input/event1:      rotary@1
+#/dev/input/event2:      rotary@1_button
+export QT_QPA_GENERIC_PLUGINS=evdevmouse:/dev/input/event0:evdevmouse:/dev/input/event1:evdevkeyboard:/dev/input/event2
+
+#Env touchscreen
+#export QT_QPA_FB_TSLIB=1
+#export TSLIB_TSDEVICE=/dev/input/event0
+#export QT_QPA_GENERIC_PLUGINS=tslib:/dev/input/event0
 
 # export FB_MULTI_BUFFER=2
