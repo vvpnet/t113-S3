8b1ce724262325f650109082afac504ebc935626
Jookia <contact@jookia.org>
2024-01-06 21:05:49 +1100

    Enable ftrace and mount /sys/kernel/debug

--- board/mangopi/sun8i-t113/script/post-build.sh
+++ board/mangopi/sun8i-t113/script/post-build.sh
@@ -4,6 +4,8 @@ rm $1/etc/resolv.conf
 echo "nameserver 8.8.8.8" > $1/etc/resolv.conf
 echo "nameserver 4.2.2.4" >> $1/etc/resolv.conf
 
+echo "none		/sys/kernel/debug		debugfs	defaults	0	0" >> $1/etc/fstab
+
 cp $BOARD_DIR/kernel.its $BINARIES_DIR/kernel.its
 (cd $BINARIES_DIR; $HOST_DIR/bin/mkimage -f kernel.its kernel.itb; rm kernel.its)
 
