a671cc55e113491c4b9c4665b1a42a143128edf1
vvpnet <v.v.p.net@mail.ru>
2024-04-04 13:35:19 +0600

    Fix settings package

--- package/nova-wi/nova-wi.mk
+++ package/nova-wi/nova-wi.mk
@@ -6,12 +6,24 @@
 
 NOVA_WI_SITE = ssh://git@github.com/Coxwain1987/t113_web_interface.git
 NOVA_WI_SITE_METHOD = git
-NOVA_WI_VERSION = v1.0.1
+NOVA_WI_VERSION = device_01_app_v1
 
 # install
 define NOVA_WI_INSTALL_TARGET_CMDS
-	$(INSTALL) -d $(@D)/etc $(TARGET_DIR)/etc
-	$(INSTALL) -d $(@D)/var/www $(TARGET_DIR)/var/www
+	$(INSTALL) -D -m 0644 $(@D)/etc/nginx/nginx.conf $(TARGET_DIR)/etc/nginx/nginx.conf
+	$(INSTALL) -D -m 0644 $(@D)/etc/nginx/fastcgi.conf $(TARGET_DIR)/etc/nginx/fastcgi.conf
+	$(INSTALL) -D -m 0644 $(@D)/etc/nginx/.htpasswd $(TARGET_DIR)/etc/nginx/.htpasswd
+	$(INSTALL) -D -m 0644 $(@D)/etc/nginx/fastcgi_params $(TARGET_DIR)/etc/nginx/fastcgi_params
+	$(Q)cp -f -r -v $(@D)/var/www $(TARGET_DIR)/var
+endef
+
+define NOVA_WI_PERMISSIONS
+	/var/www r -1 www-data www-data  - - - - -
+endef
+
+define NOVA_WI_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 $(@D)/etc/init.d/S45wifi $(TARGET_DIR)/etc/init.d/S45wifi
+	$(INSTALL) -D -m 0755 $(@D)/etc/init.d/S60fcgiwrap $(TARGET_DIR)/etc/init.d/S60fcgiwrap
 endef
 
 $(eval $(generic-package))
