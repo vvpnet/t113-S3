990a2217e29560dae6ad19fb3b9947c6e48f2da3
vvpnet <v.v.p.net@mail.ru>
2024-07-18 14:39:21 +0600

    Adding the necessary peripherals.

---
+++ board/mangopi/sun8i-t113/overlay/etc/profile.d/profile.sh
@@ -0,0 +1,18 @@
+export LANG=ru_RU.UTF-8
+export PATH=/root:$PATH
+export GST_INSPECT_NO_COLORS=1
+
+export QT_QPA_FB_DRM=0
+export QT_QPA_PLATFORM=linuxfb:fb=/dev/fb0:size=640x480:mmsize=120x120
+#Available devices:
+#/dev/input/event0:      rotary@0
+#/dev/input/event1:      rotary@1
+#/dev/input/event2:      rotary@1_button
+export QT_QPA_GENERIC_PLUGINS=evdevmouse:/dev/input/event0:evdevmouse:/dev/input/event1:evdevkeyboard:/dev/input/event2
+
+#Env touchscreen
+#export QT_QPA_FB_TSLIB=1
+#export TSLIB_TSDEVICE=/dev/input/event0
+#export QT_QPA_GENERIC_PLUGINS=tslib:/dev/input/event0
+
+# export FB_MULTI_BUFFER=2
