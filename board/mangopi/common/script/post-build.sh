882f53ad2fb27d807e19258f0022e6be7a0f275f
Jookia <contact@jookia.org>
2024-09-27 16:50:37 +1000

    recovery: Fix initrd not building in to kernel image

--- board/mangopi/common/script/post-build.sh
+++ board/mangopi/common/script/post-build.sh
@@ -6,10 +6,4 @@ echo "nameserver 4.2.2.4" >> $1/etc/resolv.conf
 
 echo "none		/sys/kernel/debug		debugfs	defaults	0	0" >> $1/etc/fstab
 
-if test -e $BINARIES_DIR/rootfs.cpio.xz ; then
-	cp $BOARD_DIR/kernel_initrd.its $BINARIES_DIR/kernel.its
-else 
-	cp $BOARD_DIR/kernel.its $BINARIES_DIR/kernel.its
-fi
-
 echo "$BRD end"
